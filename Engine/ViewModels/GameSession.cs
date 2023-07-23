442389f30c2a6a54614984ce8c41f51ec928de6b
Emyr Griffiths <emyr.griffiths@gmail.com>
2023-07-23 20:12:31 +0100

    GameSession class added

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Emyr";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
