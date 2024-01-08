2885bce58b2d3c57ab71c84e92504d95c3099175
Emyr Griffiths <emyr.griffiths@gmail.com>
2024-01-08 21:32:42 +0000

    7m20s

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Factories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,5 +15,57 @@ namespace Engine.Models
         public string Description { get; set; }
         public string ImageName { get; set; }
         public List<Quest> QuestsAvailableHere { get; set; } = new List<Quest>();
+
+        public List<MonsterEncounter> MonstersHere { get; set;} = 
+            new List<MonsterEncounter>();
+
+        public void AddMonster(int monsterId, int chanceOfEncountering)
+        {
+            if (MonstersHere.Exists(m => m.MonsterId == monsterId))
+            {
+                // Monster has already been added to this location.
+                // So, overwrite the ChanceOfEncountering with the new number.
+                MonstersHere.First(m => m.MonsterId == monsterId)
+                    .ChanceOfEncountering = chanceOfEncountering;
+            }
+            else
+            {
+                // This monster is not already at this location, so add it.
+                MonstersHere.Add(new MonsterEncounter(monsterId, chanceOfEncountering));
+            }
+        }
+
+        public Monster GetMonster()
+        {
+            if (!MonstersHere.Any())
+            {
+                return null;
+            }
+
+            // total the percentages of all monsters at this location
+            int totalChances = MonstersHere.Sum(m => m.ChanceOfEncountering);
+
+            // Select a random number between 1 and the total (in case the total chances is not 100)
+            int randomNumber = RandomNumberGenerator.NumberBetween(1, totalChances);
+
+            // Loop through the monster list
+            // adding the monsters percentage change of appearing to the runningTotal variable.
+            // When the ranom number is lower that the runningTotal, 
+            // that is the monster to return. 
+            int runningTotal = 0;
+
+            foreach (var monsterEncounter in MonstersHere)
+            {
+                runningTotal += monsterEncounter.ChanceOfEncountering;
+
+                if(randomNumber <= runningTotal)
+                {
+                    return MonsterFactory.GetMonster(monsterEncounter.MonsterId);
+                }
+            }
+
+            // If there was a problem, return the last monster in the list
+            return MonsterFactory.GetMonster(MonstersHere.Last().MonsterId);
+        }
     }
 }
